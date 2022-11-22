b06856b7df4a54118df947d5ee340f6e1e8e9a62
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-11-22 19:30:55 -0300

    feat: add success and error messages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 import { DisplayComponent } from './components/display/display.component';
+import { SuccessComponent } from './components/success/success.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
     InputComponent,
-    DisplayComponent
+    DisplayComponent,
+    SuccessComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule
