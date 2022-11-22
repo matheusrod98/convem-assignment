68c3e4f70b0ee01d45ff4f88cc4fe0fb579f0c99
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-11-22 19:21:41 -0300

    feat: add basic html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
+import { DisplayComponent } from './components/display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    InputComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/display/display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.css']
+})
+export class DisplayComponent {
+    question: string = "Você quer se juntar ao time da Convem?"
+
+}
